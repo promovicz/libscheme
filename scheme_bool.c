3c0e2920e139524f575e0c453e3ec7bca9665b6a
Ingo Albrecht <commit@promovicz.org>
2021-10-17 22:25:56 +0200

    Cleanup

--- scheme_bool.c
+++ scheme_bool.c
@@ -31,18 +31,18 @@ Scheme_Value scheme_false;
 Scheme_Value scheme_true_type;
 Scheme_Value scheme_false_type;
 
-/* locals */
-static Scheme_Value scheme_make_true (void);
-static Scheme_Value scheme_make_false (void);
+/* primitive declarations */
 static Scheme_Value not_prim (int argc, Scheme_Value argv[]);
 static Scheme_Value boolean_p_prim (int argc, Scheme_Value argv[]);
 static Scheme_Value eq_prim (int argc, Scheme_Value argv[]);
 static Scheme_Value eqv_prim (int argc, Scheme_Value argv[]);
 static Scheme_Value equal_prim (int argc, Scheme_Value argv[]);
 
-/* internals */
+/* internal declarations */
 static int list_equal (Scheme_Value lst1, Scheme_Value lst2);
 static int vector_equal (Scheme_Value vec1, Scheme_Value vec2);
+
+/* exported functions */
 
 void
 scheme_init_bool (Scheme_Env *env)
@@ -51,8 +51,8 @@ scheme_init_bool (Scheme_Env *env)
   scheme_false_type = scheme_make_type ("<false>");
   scheme_add_global ("<true>", scheme_true_type, env);
   scheme_add_global ("<false>", scheme_false_type, env);
-  scheme_true = scheme_make_true ();
-  scheme_false = scheme_make_false ();
+  scheme_true = scheme_alloc_object(scheme_true_type, 0);
+  scheme_false = scheme_alloc_object(scheme_false_type, 0);
   scheme_add_global ("not", scheme_make_prim (not_prim), env);
   scheme_add_global ("boolean?", scheme_make_prim (boolean_p_prim), env);
   scheme_add_global ("eq?", scheme_make_prim (eq_prim), env);
@@ -60,17 +60,92 @@ scheme_init_bool (Scheme_Env *env)
   scheme_add_global ("equal?", scheme_make_prim (equal_prim), env);
 }
 
-static Scheme_Value
-scheme_make_true (void)
+SCHEME_FUN_CONST
+int
+scheme_eq (Scheme_Value obj1, Scheme_Value obj2)
 {
-  return scheme_alloc_object(scheme_true_type, 0);
+  return (obj1 == obj2);
 }
 
-static Scheme_Value
-scheme_make_false (void)
+SCHEME_FUN_PURE
+int
+scheme_eqv (Scheme_Value obj1, Scheme_Value obj2)
 {
-  return scheme_alloc_object(scheme_false_type, 0);
+  if (obj1 == obj2)
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) != SCHEME_TYPE(obj2))
+    {
+      return 0;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_integer_type &&
+	   SCHEME_INT_VAL(obj1) == SCHEME_INT_VAL(obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1)== scheme_double_type &&
+	   SCHEME_DBL_VAL(obj1) == SCHEME_DBL_VAL(obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_char_type &&
+	   SCHEME_CHAR_VAL(obj1) == SCHEME_CHAR_VAL(obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_pointer_type &&
+	   SCHEME_PTR_VAL(obj1) == SCHEME_PTR_VAL(obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_SYMBOLP(obj1))
+    {
+      if (strcmp (SCHEME_STR_VAL(obj1), SCHEME_STR_VAL(obj2)) == 0)
+	return 1;
+      else
+	return 0;
+    }
+  else
+    {
+      return 0;
+    }
 }
+
+SCHEME_FUN_PURE
+int
+scheme_equal (Scheme_Value obj1, Scheme_Value obj2)
+{
+  if (scheme_eqv (obj1, obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) != SCHEME_TYPE(obj2))
+    {
+      return 0;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_pair_type &&
+	   list_equal(obj1, obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_vector_type &&
+	   vector_equal(obj1, obj2))
+    {
+      return 1;
+    }
+  else if (SCHEME_TYPE(obj1) == scheme_string_type &&
+	   (strcmp(SCHEME_STR_VAL(obj1), SCHEME_STR_VAL(obj2)) == 0))
+    {
+      return 1;
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+/* primitive functions */
 
 static Scheme_Value
 not_prim (int argc, Scheme_Value argv[])
@@ -142,90 +217,7 @@ equal_prim (int argc, Scheme_Value argv[])
     }
 }
 
-SCHEME_FUN_CONST
-int
-scheme_eq (Scheme_Value obj1, Scheme_Value obj2)
-{
-  return (obj1 == obj2);
-}
-
-SCHEME_FUN_PURE
-int
-scheme_eqv (Scheme_Value obj1, Scheme_Value obj2)
-{
-  if (obj1 == obj2)
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) != SCHEME_TYPE(obj2))
-    {
-      return 0;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_integer_type &&
-           SCHEME_INT_VAL(obj1) == SCHEME_INT_VAL(obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1)== scheme_double_type &&
-           SCHEME_DBL_VAL(obj1) == SCHEME_DBL_VAL(obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_char_type &&
-           SCHEME_CHAR_VAL(obj1) == SCHEME_CHAR_VAL(obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_pointer_type &&
-           SCHEME_PTR_VAL(obj1) == SCHEME_PTR_VAL(obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_SYMBOLP(obj1))
-    {
-      if (strcmp (SCHEME_STR_VAL(obj1), SCHEME_STR_VAL(obj2)) == 0)
-        return 1;
-      else
-        return 0;
-    }
-  else
-    {
-      return 0;
-    }
-}
-
-SCHEME_FUN_PURE
-int
-scheme_equal (Scheme_Value obj1, Scheme_Value obj2)
-{
-  if (scheme_eqv (obj1, obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) != SCHEME_TYPE(obj2))
-    {
-      return 0;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_pair_type &&
-           list_equal(obj1, obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_vector_type &&
-           vector_equal(obj1, obj2))
-    {
-      return 1;
-    }
-  else if (SCHEME_TYPE(obj1) == scheme_string_type &&
-           (strcmp(SCHEME_STR_VAL(obj1), SCHEME_STR_VAL(obj2)) == 0))
-    {
-      return 1;
-    }
-  else
-    {
-      return 0;
-    }
-}
+/* internal functions */
 
 static int
 list_equal (Scheme_Value lst1, Scheme_Value lst2)
@@ -237,7 +229,7 @@ list_equal (Scheme_Value lst1, Scheme_Value lst2)
   else
     {
       return (scheme_equal (SCHEME_CAR (lst1), SCHEME_CAR (lst2)) &&
-              scheme_equal (SCHEME_CDR (lst1), SCHEME_CDR (lst2)));
+	      scheme_equal (SCHEME_CDR (lst1), SCHEME_CDR (lst2)));
     }
 }
 
@@ -249,9 +241,9 @@ vector_equal (Scheme_Value vec1, Scheme_Value vec2)
   for ( i=0 ; i<SCHEME_VEC_SIZE(vec1) ; ++i )
     {
       if (! scheme_equal (SCHEME_VEC_ELS(vec1)[i], SCHEME_VEC_ELS(vec2)[i]))
-        {
-          return 0;
-        }
+	{
+	  return 0;
+	}
     }
   return 1;
 }
